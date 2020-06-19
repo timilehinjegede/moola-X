d1df442b886a3cfa29084ff8754a7816d27948ee
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-19 18:19:19 +0100

    Fixed import errors

--- lib/services/currency/currency_service_implementation.dart
+++ lib/services/currency/currency_service_implementation.dart
@@ -1,44 +1,13 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-import 'package:moolax/business_logic/models/currency.dart';
-import 'package:moolax/business_logic/models/rate.dart';
-import 'package:moolax/services/storage/storage_service.dart';
-import 'package:moolax/services/web_api/web_api.dart';
-import 'package:moolax/services/service_locator.dart';
-
-import 'currency_service.dart';
-
 // This class is the concrete implementation of [CurrencyService]. It is a
 // wrapper around the WebApi and StorageService services. This way the view models
 // don't actually have to know anything about the web or storage details.
+import 'package:moola_x/business_logic/models/currency.dart';
+import 'package:moola_x/business_logic/models/rate.dart';
+import 'package:moola_x/services/currency/currency_service.dart';
+import 'package:moola_x/services/service_locator.dart';
+import 'package:moola_x/services/storage/storage_service.dart';
+import 'package:moola_x/services/web_api/web_api.dart';
+
 class CurrencyServiceImpl implements CurrencyService {
   WebApi _webApi = serviceLocator<WebApi>();
   StorageService _storageService = serviceLocator<StorageService>();
--- lib/services/storage/storage_service_implementation.dart
+++ lib/services/storage/storage_service_implementation.dart
@@ -1,45 +1,15 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-import 'dart:convert';
-import 'package:moolax/business_logic/models/currency.dart';
-import 'package:moolax/business_logic/models/rate.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'storage_service.dart';
-
 // This class is the concrete implementation of [StorageService]. Internally
 // it uses shared preferences to save and retrieve data, but that is an
 // implementation detail that no other class in the app knows about. Swapping
 // out shared preferences with a SQL database would be as simple as creating
 // a new [StorageService] implementation.
+import 'dart:convert';
+
+import 'package:moola_x/business_logic/models/currency.dart';
+import 'package:moola_x/business_logic/models/rate.dart';
+import 'package:moola_x/services/storage/storage_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class StorageServiceImpl implements StorageService {
 
   static const sharedPrefExchangeRateKey = 'exchange_rate_key';
