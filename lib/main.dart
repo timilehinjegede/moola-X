1c62a0492eae2552a41f51009a7145fe870f71be
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-21 02:47:40 +0100

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Moola X',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
