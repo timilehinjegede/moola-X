f797d599b34f629c2736ec23fc064053822a1990
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-21 02:22:05 +0100

    Choose favourite view model finished

--- lib/business_logic/view_models/choose_favorites_viewmodel.dart
+++ lib/business_logic/view_models/choose_favorites_viewmodel.dart
@@ -1,33 +1,73 @@
-
-// 2
 import 'package:flutter/foundation.dart';
 import 'package:moola_x/business_logic/models/currency.dart';
+import 'package:moola_x/business_logic/models/rate.dart';
+import 'package:moola_x/business_logic/utils/iso_data.dart';
 import 'package:moola_x/services/currency/currency_service.dart';
 import 'package:moola_x/services/service_locator.dart';
 
 class ChooseFavoritesViewModel extends ChangeNotifier {
 
-  // 3
   final CurrencyService _currencyService = serviceLocator<CurrencyService>();
 
   List<FavoritePresentation> _choices = [];
   List<Currency> _favorites = [];
 
-  // 4
   List<FavoritePresentation> get choices => _choices;
 
   void loadData() async {
-    // ...
-
-    // 5
+    final rates = await _currencyService.getAllExchangeRates();
+    _favorites = await _currencyService.getFavoriteCurrencies();
+    _prepareChoicePresentation(rates);
     notifyListeners();
   }
 
-  void toggleFavoriteStatus(int choiceIndex) {
-    // ...
+  void _prepareChoicePresentation(List<Rate> rates) {
+    List<FavoritePresentation> list = [];
+    for (Rate rate in rates) {
+      String code = rate.quoteCurrency;
+      bool isFavorite = _getFavoriteStatus(code);
+      list.add(FavoritePresentation(
+        flag: IsoData.flagOf(code),
+        alphabeticCode: code,
+        longName: IsoData.longNameOf(code),
+        isFavorite: isFavorite,
+      ));
+    }
+    _choices = list;
+  }
 
-    // 5
+  bool _getFavoriteStatus(String code) {
+    for (Currency currency in _favorites) {
+      if (code == currency.isoCode)
+        return true;
+    }
+    return false;
+  }
+
+  void toggleFavoriteStatus(int choiceIndex) {
+    final isFavorite = !_choices[choiceIndex].isFavorite;
+    final code = _choices[choiceIndex].alphabeticCode;
+    _choices[choiceIndex].isFavorite = isFavorite;
+    if (isFavorite) {
+      _addToFavorites(code);
+    } else {
+      _removeFromFavorites(code);
+    }
     notifyListeners();
+  }
+
+  void _addToFavorites(String alphabeticCode) {
+    _favorites.add(Currency(alphabeticCode));
+    _currencyService.saveFavoriteCurrencies(_favorites);
+  }
+
+  void _removeFromFavorites(String alphabeticCode) {
+    for (final currency in _favorites) {
+      if (currency.isoCode == alphabeticCode) {
+        _favorites.remove(currency);
+        break;
+      }
+    }
   }
 }
 
