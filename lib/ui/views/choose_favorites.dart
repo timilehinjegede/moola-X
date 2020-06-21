ba98c1709ed6611ae8b1ea2113d59ca106ef6509
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-21 02:46:15 +0100

    Choose favorites currency service added

--- lib/ui/views/choose_favorites.dart
+++ lib/ui/views/choose_favorites.dart
@@ -1,39 +1,68 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'package:flutter/material.dart';
+import 'package:moola_x/business_logic/view_models/choose_favorites_viewmodel.dart';
+import 'package:moola_x/services/service_locator.dart';
+import 'package:provider/provider.dart';
 
-class ChooseFavoriteCurrencyScreen extends StatelessWidget {
+class ChooseFavoriteCurrencyScreen extends StatefulWidget {
+
+  @override
+  _ChooseFavoriteCurrencyScreenState createState() => _ChooseFavoriteCurrencyScreenState();
+}
+
+class _ChooseFavoriteCurrencyScreenState extends State<ChooseFavoriteCurrencyScreen> {
+
+  ChooseFavoritesViewModel viewModel = serviceLocator<ChooseFavoritesViewModel>();
+
+  @override
+  void initState() {
+    viewModel.loadData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose Currencies'),
+      ),
+      body: buildListView(viewModel),
+    );
+  }
+
+  Widget buildListView(ChooseFavoritesViewModel viewModel) {
+    // 1
+    return ChangeNotifierProvider<ChooseFavoritesViewModel>(
+      // 2
+      create: (context) => viewModel,
+      // 3
+      child: Consumer<ChooseFavoritesViewModel>(
+        builder: (context, model, child) => ListView.builder(
+          itemCount: model.choices.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: SizedBox(
+                  width: 60,
+                  child: Text(
+                    '${model.choices[index].flag}',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                // 4
+                title: Text('${model.choices[index].alphabeticCode}'),
+                subtitle: Text('${model.choices[index].longName}'),
+                trailing: (model.choices[index].isFavorite)
+                    ? Icon(Icons.favorite, color: Colors.red)
+                    : Icon(Icons.favorite_border),
+                onTap: () {
+                  // 5
+                  model.toggleFavoriteStatus(index);
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
